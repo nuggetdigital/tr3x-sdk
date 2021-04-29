05b20ad7929945804cf853bd6d441f3b11bcefc7
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-04-29 14:04:58 +0200

    fix: a require err deep in ipfs-core's dep tree by usin @babel/register

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// NOTE: usin @babel/register's magic here to workaround an ipfs-core dep err
+require("@babel/register")({
+  // This will override `node_modules` ignoring - you can alternatively pass
+  // an array of strings to be explicitly matched or a regex / glob
+  ignore: [],
+})
+
 module.exports = {
   metadata: require("./metadata"),
   initIPFS: require("./ipfs"),
