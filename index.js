cacc814c68577baf4d2043d1f4e60533148e2f6a
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-05-21 12:44:15 +0200

    impl: just export our conv func

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { init, hash256hex } from "blake3-wasm-sync"
 export const metadata = _metadata
 // export const initIpfs = _initIpfs
 
+export const blake3hash256hex = hash256hex
+
 export async function blake3() {
   await init()
   return hash256hex
