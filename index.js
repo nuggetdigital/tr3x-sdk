a6961763fc6797cbb5c94d0ff235e0414b7cb2d3
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-05-21 12:36:07 +0200

    impl: got rid of an now unnecessary check

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ import { init, hash256hex } from "blake3-wasm-sync"
 export const metadata = _metadata
 // export const initIpfs = _initIpfs
 
-let _blake3_init = false
-
 export async function blake3() {
-  if (!_blake3_init) {
-    await init()
-    _blake3_init = true
-  }
+  await init()
   return hash256hex
 }
