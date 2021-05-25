f45c21047d17bbc8b72f577f6bf866e84385193c
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-05-25 15:06:31 +0200

    impl: only nl between paragraphs in the license text

--- test.js
+++ test.js
@@ -36,25 +36,13 @@ tape("assembles valid params to lease metadata", t => {
     license: `
 tr3x public performance lease license
 
-Permission is hereby granted, at a charge of ${price}STYC (TR3X), 
-payable to ${network} network address ${payee}, to any person purchasing 
-a token of this digital license asset to perform the associated track 
-named "${title}", © ${copyrightYear} ${artist}, identified by its BLAKE3 
-256-bit hash digest 0x${blake3256}, in public, for a lease term of ${term} 
-finalized blocks on the ${network} network, starting with the block number 
-that the purchase transaction acquiring this license got finalized in.
+Permission is hereby granted, at a charge of ${price}STYC (TR3X), payable to ${network} network address ${payee}, to any person purchasing a token of this digital license asset to perform the associated track named "${title}", © ${copyrightYear} ${artist}, identified by its BLAKE3 256-bit hash digest 0x${blake3256}, in public, for a lease term of ${term} finalized blocks on the ${network} network, starting with the block number that the purchase transaction acquiring this license got finalized in.
 
-Maximum profits off of public performances of the lessee must not excceed 
-${cap}€, otherwise the lessee must monthly payback 50% of the excess 
-profits to above payee via the marketplace in TR3X at the EUR/TR3X payback 
-rate of ${paybackRateEURTR3X}.
+Maximum profits off of public performances of the lessee must not excceed ${cap}€, otherwise the lessee must monthly payback 50% of the excess profits to above payee via the marketplace in TR3X at the EUR/TR3X payback rate of ${paybackRateEURTR3X}.
 
-The artist name "${artist}" must be visibly included in all digital and 
-physical copies and noticeably mentioned at any public performances 
-explicitely accrediting ${artist} as the creator of "${title}".
+The artist name "${artist}" must be visibly included in all digital and physical copies and noticeably mentioned at any public performances explicitely accrediting ${artist} as the creator of "${title}".
 
-Claims of this license must be prooved using tr3x purchase transactions on 
-the ${network} network.
+Claims of this license must be prooved using tr3x purchase transactions on the ${network} network.
       `.trim()
   }
 
@@ -102,19 +90,11 @@ tape("assembles valid params to exclusive metadata", t => {
     license: `
 tr3x public performance exclusive license
 
-Permission is hereby granted, at a charge of ${price}STYC (TR3X), 
-payable to ${network} network address ${payee}, to the first person 
-purchasing a token of this digital license asset to exclusively 
-perform the associated track named "${title}", © ${copyrightYear} 
-${artist}, identified by its BLAKE3 256-bit hash digest 0x${blake3256}, 
-in public.
+Permission is hereby granted, at a charge of ${price}STYC (TR3X), payable to ${network} network address ${payee}, to the first person purchasing a token of this digital license asset to exclusively perform the associated track named "${title}", © ${copyrightYear} ${artist}, identified by its BLAKE3 256-bit hash digest 0x${blake3256}, in public.
 
-The artist name "${artist}" must be visibly included in all digital and 
-physical copies and noticeably mentioned at any public performances 
-explicitely accrediting ${artist} as the creator of "${title}".
+The artist name "${artist}" must be visibly included in all digital and physical copies and noticeably mentioned at any public performances explicitely accrediting ${artist} as the creator of "${title}".
 
-Claims of this particular license must be verified against their respective 
-purchases on the ${network} network.
+Claims of this particular license must be verified against their respective purchases on the ${network} network.
     `.trim()
   }
 
