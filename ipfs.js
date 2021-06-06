d077d3b73c45022d712f799c601e55cf29764575
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-06-06 09:11:42 +0200

    fix: use getHeaders in node

--- ipfs.js
+++ ipfs.js
@@ -1,20 +1,18 @@
 import FormData from "form-data"
 import { Buffer } from "buffer"
 
-globalThis.Buffer = Buffer
-globalThis.FormData = FormData
-
 export default function init(baseUrl) {
   baseUrl = baseUrl.replace(/\/+$/, "")
   return {
     async add(buf) {
-      const formdata = new FormData(/*{ maxDataSize: 5e8 }*/) // 500 MB
+      const formdata = new FormData()
       formdata.append("file", Buffer.from(buf))
       let res = await fetch(
         `${baseUrl}/add?cid-version=1&hash=blake2b-256&pin=false`,
         {
           method: "POST",
           body: formdata,
+          headers: formdata?.getHeaders()
         }
       )
       if (res.status !== 200) {
